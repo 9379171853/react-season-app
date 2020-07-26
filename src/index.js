e1f802092dfd5dfbcf8970424a5cb2e85a59072b
Amrendra Kumar <amarmca51@gmail.com>
2020-07-26 23:56:41 +0530

    basic practice class component life cycle and setstate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = { lat: null, errMessage: '' };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
+    );
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  renderContent() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>{this.state.errMessage}</div>;
+    }
+    if (!this.state.errMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message='Please accept location request' />;
+  }
+
+  render() {
+    return <div className='border red'>{this.renderContent()}</div>;
+  }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
